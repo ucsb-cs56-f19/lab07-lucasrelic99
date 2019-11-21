13672028c9cfe93cc1000db4b328511362035424
Lucas Relic <lucasrelic@umail.ucsb.edu>
2019-11-20 22:09:25 -0800

    lr - completed up to 8g. computer dying so pushing to save changes

---
+++ src/main/java/hello/EqSearch.java
@@ -0,0 +1,19 @@
+package hello;
+
+public class EqSearch {
+
+  private int distance;
+  private int minmag;
+
+  EqSearch () {
+    //no arg constructor
+  }
+
+  //getters
+  public int getDistance () { return distance; }
+  public int getMinmag () { return minmag; }
+
+  //setters
+  public void setDistance (int d) { distance = d; }
+  public void setMinmag (int m) { minmag = m; }
+  }
