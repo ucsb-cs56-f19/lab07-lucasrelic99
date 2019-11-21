22ad146676805e47412b4f4eba2a627ed27714b2
Lucas Relic <lucasrelic@ucsb.edu>
2019-11-20 23:23:23 -0800

    lr - added placeholder API call and wired to results page

--- src/main/java/hello/WebController.java
+++ src/main/java/hello/WebController.java
@@ -58,8 +58,10 @@ public class WebController {
 
     @GetMapping("/earthquakes/results")
     public String getEarthquakesResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken, EqSearch eqSearch) {
+        EarthquakeQueryService e = new EarthquakeQueryService();
         model.addAttribute("eqSearch", eqSearch);
-        //TODO: Actually do the search here and add results to model
+        String json = e.getJSON(eqSearch.getDistance(), eqSearch.getMinmag());
+        model.addAttribute("json", json);
         return "earthquakes/results";
     }
 }
